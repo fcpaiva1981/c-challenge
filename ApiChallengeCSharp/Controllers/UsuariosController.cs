cffdd66e1ec2780c437f1412cee626acae5c86d7
Fernando Campos de Paiva <fernandocampospaiva@hotmail.com>
2024-05-05 20:30:30 -0300

    Controller Usuário
    Todas as funções implemntadas

--- ApiChallengeCSharp/Controllers/UsuariosController.cs
+++ ApiChallengeCSharp/Controllers/UsuariosController.cs
@@ -1,45 +1,58 @@
 ﻿using ApiChallengeCSharp.Model;
+using ApiChallengeCSharp.Repository.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
- 
+
 namespace ApiChallengeCSharp.Controllers;
+
 [Route("api/[controller]")]
 [ApiController]
 public class UsuariosController : ControllerBase
 {
-     
+    private readonly IUsuarioRepository _usuarioRepository;
+
+    public UsuariosController(IUsuarioRepository usuarioRepository)
+    {
+        _usuarioRepository = usuarioRepository;
+    }
+
     [HttpGet]
-    public IEnumerable<UsuarioModel> Get()
+    public async Task<ActionResult<IEnumerable<UsuarioModel>>> BuscarTodosUsuarios()
     {
-        List<UsuarioModel> usuarioModels = new List<UsuarioModel>();
-
-        usuarioModels.Add(new UsuarioModel() { Id =1, Nome="Fernando",Email= "fcpaiva1981@gmail.com" });
-        return usuarioModels;
+        List<UsuarioModel> usuarioModels = await _usuarioRepository.BuscarTodooUsuarios();
+        return Ok(usuarioModels);
     }
 
-     
+
     [HttpGet("{id}")]
-    public UsuarioModel Get(int id)
+    public async Task<ActionResult<UsuarioModel>> BuscarPorId(int id)
     {
-        UsuarioModel usuarioModel = new UsuarioModel() { Id = 1, Nome = "Fernando", Email = "fcpaiva1981@gmail.com" };
-        return usuarioModel;
+        UsuarioModel usuarioModel = await _usuarioRepository.BuscarPorId(id);
+        return Ok(usuarioModel);
     }
 
-   
+
     [HttpPost]
-    public void Post([FromBody] UsuarioModel usuario)
+    public async Task<ActionResult<UsuarioModel>> Cadastrar([FromBody] UsuarioModel usuario)
     {
+        UsuarioModel usuarioModel = await _usuarioRepository.Adicionar(usuario);
+        return Ok(usuarioModel);
     }
 
-    
+
     [HttpPut("{id}")]
-    public void Put(int id, [FromBody] UsuarioModel usuario)
+    public async Task<ActionResult<UsuarioModel>> Atualizar(int id, [FromBody] UsuarioModel usuario)
     {
+        usuario.Id = id;
+        UsuarioModel usuarioModel = await _usuarioRepository.Atualizar(usuario, id);
+        return Ok(usuarioModel);
     }
 
-     
+
     [HttpDelete("{id}")]
-    public void Delete(int id)
+    public async Task<ActionResult<bool>> Delete(int id)
     {
+        bool apagado = await _usuarioRepository.Apagar(id);
+        return Ok(apagado);
     }
 }
