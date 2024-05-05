7983498fb21f88f3c0a1d6c180b70bed1a4581e4
Fernando Campos de Paiva <fernandocampospaiva@hotmail.com>
2024-05-05 20:43:50 -0300

    Mirgationon atualização de campos de vinculos

--- ApiChallengeCSharp/Migrations/20240505234320_VinculoPermissaUsuario.Designer.cs
+++ ApiChallengeCSharp/Migrations/20240505234320_VinculoPermissaUsuario.Designer.cs
@@ -4,6 +4,7 @@ using ApiChallengeCSharp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ApiChallengeCSharp.Migrations
 {
     [DbContext(typeof(ChallengeDbContext))]
-    partial class ChallengeDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240505234320_VinculoPermissaUsuario")]
+    partial class VinculoPermissaUsuario
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ApiChallengeCSharp/Migrations/20240505234320_VinculoPermissaUsuario.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiChallengeCSharp.Migrations
+{
+    /// <inheritdoc />
+    public partial class VinculoPermissaUsuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UsuarioId",
+                table: "Permissaos",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Permissaos_UsuarioId",
+                table: "Permissaos",
+                column: "UsuarioId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Permissaos_Usuarios_UsuarioId",
+                table: "Permissaos",
+                column: "UsuarioId",
+                principalTable: "Usuarios",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Permissaos_Usuarios_UsuarioId",
+                table: "Permissaos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Permissaos_UsuarioId",
+                table: "Permissaos");
+
+            migrationBuilder.DropColumn(
+                name: "UsuarioId",
+                table: "Permissaos");
+        }
+    }
+}
